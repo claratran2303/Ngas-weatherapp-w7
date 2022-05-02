08b0a57a7cec252376e54b9fdc68460e874ef31e
claratran2303 <88215025+claratran2303@users.noreply.github.com>
2022-05-02 15:08:29 +0700

    Change Icon

--- src/funtions.js
+++ src/funtions.js
@@ -30,6 +30,8 @@ function displayTemperature(response){
     let humidity=document.querySelector("#humidityID");
     let wind=document.querySelector("#windID");
     let datetime=document.querySelector("#datetimeID")
+    let icon=document.querySelector("#iconID");
+    let iconCode=(response.data.weather[0].icon);
     temp.innerHTML=Math.round(response.data.main.temp);
     city.innerHTML=(response.data.name);
     status.innerHTML=(response.data.weather[0].description)
@@ -37,10 +39,11 @@ function displayTemperature(response){
     humidity.innerHTML=(response.data.main.humidity);
     wind.innerHTML=Math.round(response.data.wind.speed);
     datetime.innerHTML=formatDate(response.data.dt*1000);
-    console.log(response.data.dt);
+    icon.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${iconCode}@2x.png`)
 }
-
-let city= "New York";
+let city= "Saint Petersburg";
 let apiKey="d3c8204f4c4db0d26947b9ed2cb7ac82";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
